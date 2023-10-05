90654366e9f9a1f10e05398c46e55087ed5964f0
Chris Burrows <chris.burrows@bt.com>
2023-10-05 13:02:20 +0100

    Cleanup; changed location of my_config.h in setup script

--- pio-scripts/user_config_copy.py
+++ pio-scripts/user_config_copy.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 # copy WLED00/my_config_sample.h to WLED00/my_config.h
-if os.path.isfile("wled00/my_config.h"):
+if os.path.isfile("local_customisations/my_config.h"):
     print ("*** use existing my_config.h ***")
 else: 
-    shutil.copy("wled00/my_config_sample.h", "wled00/my_config.h")
+    shutil.copy("wled00/my_config_sample.h", "local_customisations/my_config.h")
